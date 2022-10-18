46bb39f48cfa7c31f343bc3b43369358947917f1
Howard Liu <zhihliu@stonybrook.edu>
2022-10-18 09:24:45 -0400

    ctrl-Z can ctrl-Y shortcut added :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { React } from 'react'
+import { React, useContext } from 'react'
+import { GlobalStoreContext } from './store'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Banner, ListSelector, PlaylistCards, Statusbar } from './components'
 import DeleteListModal from './components/DeleteListModal';
@@ -11,6 +12,34 @@ import EditSongModal from './components/EditSongModal';
     @author McKilla Gorilla
 */
 const App = () => {
+    const { store } = useContext(GlobalStoreContext);
+    let ctrlPressed = false;
+
+    let handleAppKeyDown = (keyEvent) => {
+        let CTRL_KEY_CODE = "17";
+        if (keyEvent.which == CTRL_KEY_CODE) {
+            ctrlPressed = true;
+        }
+        else if (keyEvent.key.toLowerCase() == "z") {
+            if (ctrlPressed) {
+                store.undo();
+            }
+        }
+        else if (keyEvent.key.toLowerCase() == "y") {
+            if (ctrlPressed) {
+                store.redo();
+            }
+        }
+    }
+    
+    let handleAppKeyUp = (keyEvent) => {
+        if (keyEvent.which == "17")
+            ctrlPressed = false;
+    }
+
+    document.onkeydown = handleAppKeyDown;
+    document.onkeyup = handleAppKeyUp;
+
     return (
         <Router>
             <Banner />
